44cf9d9eac716519fac6b5189ca5873f5702daff
iyed-benali <aveyrokepler86@gmail.com>
2023-11-06 11:21:07 +0100

    done

--- back/app.js
+++ back/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const database = require('./src/config/database')
 const app = express()
+const Buyer = require('./src/models/buyerModel')
+const Product = require('./src/models/productModel')
+const Order = require('./src/models/orderModel')
 
 
 const PORT = process.env.PORT || 3001
---
+++ back/src/models/buyerModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const buyerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  orders: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Order',
+  }],
+});
+
+const Buyer = mongoose.model('Buyer', buyerSchema);
+
+module.exports = Buyer;
